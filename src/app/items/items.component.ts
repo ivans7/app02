3ac72dac4a3faf9f76f310668663591882f5a833
ivans <ivans7@gmail.com>
2018-12-30 10:21:18 -0800

    item added

---
+++ src/app/items/items.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Item} from '../item';
+
+@Component({
+  selector: 'app-items',
+  templateUrl: './items.component.html',
+  styleUrls: ['./items.component.css'] 
+})
+export class ItemsComponent implements OnInit {
+
+  item: Item = {
+    id:0, 
+    name: 'Default'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
