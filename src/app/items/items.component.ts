a9d5c4b412623e3bc1803a0f3b3877415fee01dd
ivans <ivans7@gmail.com>
2018-12-30 11:28:44 -0800

    display a list

--- src/app/items/items.component.ts
+++ src/app/items/items.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from '../item';
+import {ITEMS} from '../mock-items';
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
   styleUrls: ['./items.component.css'] 
 })
+
 export class ItemsComponent implements OnInit {
 
-  item: Item = {
-    id:0, 
-    name: 'Default'
-  }
-  constructor() { }
+  selectedItem : Item;
+  items = ITEMS;
 
-  ngOnInit() {
+  constructor() { }
+  ngOnInit() {  }
+
+  
+  onSelect (item : Item) : void {
+    this.selectedItem = item;
   }
 
 }
