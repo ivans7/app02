60ed77717f35917f2d0673c8c857d79fc556171a
ivans <ivans7@gmail.com>
2019-01-02 07:20:17 -0800

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
-    ItemDetailComponent
+    ItemDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
